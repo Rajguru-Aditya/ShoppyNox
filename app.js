e21bfcdc9d7438bb290d43c3b44fbb683b6e8e67
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-07-14 23:23:29 +0530

    Change card styling

--- app.js
+++ app.js
@@ -9,18 +9,51 @@ mongoose.connect("mongodb://localhost:27017/shoppinoxDB", {
   useFindAndModify: false,
 });
 
+const productSchema = {
+  name: String,
+  cost: String,
+  img: String,
+};
+
 const contactSchema = {
   name: String,
   email: String,
   message: String,
 };
 
+const Product = mongoose.model("Product", productSchema);
 const Contact = mongoose.model("Contact", contactSchema);
 
-const cost = "₹90000";
-const desc = "Gaming Laptop";
-const imgSrc =
-  "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80";
+const laptop = new Product({
+  name: "Gaming Laptop",
+  cost: "₹90000",
+  img: "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+});
+// laptop.save();
+
+const phone = new Product({
+  name: "Smartfone 366",
+  cost: "₹18000",
+  img: "https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=616&q=80",
+});
+
+const shirt = new Product({
+  name: "Nice Shirt",
+  cost: "₹3000",
+  img: "https://images.unsplash.com/photo-1602810318660-d2c46b750f88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+});
+
+const defaultProducts = [laptop, phone, shirt];
+Product.insertMany(defaultProducts, function (err) {
+  if (!err) {
+    console.log("Items added!");
+  }
+});
+
+// const cost = "₹90000";
+// const desc = "Gaming Laptop";
+// const imgSrc =
+//   "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80";
 
 const app = express();
 
@@ -30,7 +63,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { cost: cost, desc: desc, imgSrc: imgSrc });
+  Product.find({}, function (err, results) {
+    if (!err) {
+      res.render("home", { products: results });
+    }
+  });
 });
 
 app.get("/contact", function (req, res) {
